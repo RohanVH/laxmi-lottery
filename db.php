ff1ef192c178345acc839237b152196741c7845e
RohanVH <rohanvaradaraju.h@gmail.com>
2022-10-21 11:37:27 +0530

    127.0.0.1 to localhost

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try{
-  $db = new PDO('mysql:host=127.0.0.1;dbname=laxmi-lottery','root','');
+  $db = new PDO('mysql:host=localhost;dbname=laxmi-lottery','root','');
   $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
   die('<h4 style="color: #e4e4e4">Error connecting to the database. Please check app init file</h4>');
