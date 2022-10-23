93a334eda4f7dcfd51b49ce820f7ef60de48b5c6
Rohan <rohanvaradaraju.h@gmail.com>
2022-10-23 14:41:00 +0530

    all set

--- db.php
+++ db.php
@@ -1,3 +1,6 @@
+<!-- database file names:
+ 1. config.php 
+2. db. php-->
 <?php
 try{
   $db = new PDO('mysql:host=localhost;dbname=laxmi-lottery','root','');
@@ -5,7 +8,3 @@ try{
 }catch(PDOException $e){
   die('<h4 style="color: #e4e4e4">Error connecting to the database. Please check app init file</h4>');
 }
-?>
-<!-- database file names:
- 1. config.php 
-2. db. php-->
